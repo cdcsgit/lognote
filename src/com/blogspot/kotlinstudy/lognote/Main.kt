fd03d7db9256547ea9729d2d7cfb90a8abe3307c
cdcsgit <cdcsman@gmail.com>
2022-03-10 18:58:28 +0900

    version up 0.2.2

--- src/com/blogspot/kotlinstudy/lognote/Main.kt
+++ src/com/blogspot/kotlinstudy/lognote/Main.kt
@@ -7,7 +7,7 @@ import javax.swing.plaf.ColorUIResource
 
 class Main {
     companion object {
-        const val VERSION: String = "0.2.1"
+        const val VERSION: String = "0.2.2"
 
         @JvmStatic
         fun main(args: Array<String>) {
