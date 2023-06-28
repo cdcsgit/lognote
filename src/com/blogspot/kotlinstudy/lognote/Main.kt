fc44fd32e4522a0ae488fdd5fb0077c9dcc856e8
cdcsgit <cdcsman@gmail.com>
2023-06-28 22:40:58 +0900

    version up 0.3.2

--- src/com/blogspot/kotlinstudy/lognote/Main.kt
+++ src/com/blogspot/kotlinstudy/lognote/Main.kt
@@ -4,7 +4,7 @@ import javax.swing.SwingUtilities
 
 class Main {
     companion object {
-        const val VERSION: String = "0.3.1"
+        const val VERSION: String = "0.3.2"
         const val NAME: String = "LogNote"
 
         @JvmStatic
