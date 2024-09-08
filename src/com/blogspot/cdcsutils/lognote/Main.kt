ee1429119a79ff85eeaed914ab7bf24c21be0d47
cdcsgit <cdcsman@gmail.com>
2024-09-08 13:17:35 +0900

    version up 0.3.7

--- src/com/blogspot/cdcsutils/lognote/Main.kt
+++ src/com/blogspot/cdcsutils/lognote/Main.kt
@@ -4,7 +4,7 @@ import javax.swing.SwingUtilities
 
 class Main {
     companion object {
-        const val VERSION: String = "0.3.6"
+        const val VERSION: String = "0.3.7"
         const val NAME: String = "LogNote"
 
         @JvmStatic
